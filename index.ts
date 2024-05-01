91066ffade87b9dcda423858b8f5599a240b0146
--replace-all <[salmansaleemriz@gmail.com]>
2024-05-01 05:19:21 +0500

    created number guesing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+//shabang
+
+import inquirer from "inquirer";
+
+//1 computer will generate a random number
+//2 user input for guessing number
+//3 compare the both
+
+const randomnumber = Math.floor(Math.random() * 10 +1);
+
+const answer = await inquirer.prompt([
+  { name: "userguessednumber", type: "number", message: "PLZ GUESS A NUMBER BETWEEN 1-10" },
+]);
+if (answer.userguessednumber === randomnumber) {
+  console.log("CONGRATS YOU GUES A RIGHT NUMBER ");
+} else {
+  console.log("YOU GUESS THE WRONG NUMBER");
+}
